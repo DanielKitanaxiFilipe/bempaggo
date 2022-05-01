e1edc2d57186fe61eea3a227d88c7507b6e45c5d
Daniel Kitanaxi Filipe <danielkitanaxifilipe12@gmail.com>
2022-05-01 17:33:02 -0300

    site
    site

---
+++ js/inptuCheck.js
@@ -0,0 +1,31 @@
+function checkfunction() {
+  var check = document.getElementById("check");
+  var divNunber = document.getElementById("divNunber");
+  if (check.checked == true) {
+      divNunber.style.display = "block";
+  } 
+  else {
+     divNunber.style.display = "none";
+  }
+}
+function checkfunction2() {
+  var check2 = document.getElementById("check2");
+  var divNunber2 = document.getElementById("divNunber2");
+  if (check2.checked == true) {
+      divNunber2.style.display = "block";
+  } 
+  else {
+     divNunber2.style.display = "none";
+  }
+}
+function checkfunction3() {
+  var check3 = document.getElementById("check3");
+  var divNunber3 = document.getElementById("divNunber3");
+  if (check3.checked == true) {
+      divNunber3.style.display = "block";
+  } 
+  else {
+    
+     divNunber3.style.display = "none";
+  }
+}
